53512a5211e74c21359c6e7f4b064eea6bb41aca
luciano.echevarne <luciano.echevarne@LEchevarne-PC.corp.gruposlots>
2021-05-11 10:19:36 -0300

    commit1

---
+++ src/tp8/transversal/g2/TransversalTp8G2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp8.transversal.g2;
+
+/**
+ *
+ * @author luciano.echevarne
+ */
+public class TransversalTp8G2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
